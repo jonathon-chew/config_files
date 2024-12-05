1147cc577a66f2b08ca902d2e530037e9ddb83ec
Jonathon Chew <>
2024-12-05 20:14:42 +0000

    Added an echo to the bottom of build to specify the VSCode profile. Commented out init lua file around <leader>cc

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@
 
 # Run the Mac OS script to apply the settings that I like
 cd $HOME
+cp ./.zshrc . 
 mkdir .config
 
 # Set the terminal settings
 echo "When opening Terminal point to this file to get the current settings"
+echo ""
 
 # Run the brew script installing regularly used programmes and CLI tools
 ./brew.sh
 
 # Set the VS Code Setting
 echo "When opening VSCode point to this file to get the current settings"
+echo "Standard.code-profile"
