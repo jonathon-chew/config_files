d8245b71d53ea61b525eb1098ff3ec0a669b4a18
Jonathon Chew <>
2024-12-11 22:16:16 +0000

    Removing notation for how to map function to programme

--- nvim/lua/comment_lines/init.lua
+++ nvim/lua/comment_lines/init.lua
@@ -43,13 +43,13 @@ function M.comment_lines(count)
     end
 end
 
--- Create a command that calls the function
-vim.api.nvim_create_user_command(
-    "CommentLines",
-    function(opts)
-        M.comment_lines(tonumber(opts.args))
-    end,
-    { nargs = 1 } -- Requires exactly one argument (number of lines)
-)
+---- Create a command that calls the function
+--vim.api.nvim_create_user_command(
+--    "CommentLines",
+--    function(opts)
+--        M.comment_lines(tonumber(opts.args))
+--    end,
+--    { nargs = 1 } -- Requires exactly one argument (number of lines)
+--)
 
 return M
