396cf1d4e159f19aff6652690540efe4c64cb23f
Jonathon Chew <>
2025-04-21 11:06:16 +0100

    Paying around with settings to make nvim work how I want, trying to deal with issues with treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -215,14 +215,14 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
+--  {
+--    -- Highlight, edit, and navigate code
+--    'nvim-treesitter/nvim-treesitter',
+--    dependencies = {
+--      'nvim-treesitter/nvim-treesitter-textobjects',
+--    },
+--    build = ':TSUpdate',
+--  },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
@@ -280,7 +280,16 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 --vim.o.termguicolors = false
-vim.o.termguicolors = true
+--vim.o.termguicolors = true
+
+-- Detect the terminal emulator
+local term = vim.env.TERM or ""
+
+if term == "xterm-256color" then
+    vim.o.termguicolors = false
+elseif term == "ghostty" then
+    vim.o.termguicolors = true
+end
 
 -- Default formatting
 vim.opt.cursorline = true
@@ -394,75 +403,75 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'go', 'lua', 'python', 'javascript', 'vim', 'bash', 'php' },
-
-    sync_install = false;
-    modules = {};
-    ignore_install = {};
-
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
-
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
-  }
-end, 0)
+--vim.defer_fn(function()
+--  require('nvim-treesitter.configs').setup {
+--    -- Add languages to be installed here that you want installed for treesitter
+--    ensure_installed = { 'c', 'go', 'lua', 'python', 'javascript', 'vim', 'bash', 'php' },
+--
+--    sync_install = false;
+--    modules = {};
+--    ignore_install = {};
+--
+--    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+--    auto_install = false,
+--
+--    highlight = { enable = true },
+--    indent = { enable = true },
+--    incremental_selection = {
+--      enable = true,
+--      keymaps = {
+--        init_selection = '<c-space>',
+--        node_incremental = '<c-space>',
+--        scope_incremental = '<c-s>',
+--        node_decremental = '<M-space>',
+--      },
+--    },
+--    textobjects = {
+--      select = {
+--        enable = true,
+--        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+--        keymaps = {
+--          -- You can use the capture groups defined in textobjects.scm
+--          ['aa'] = '@parameter.outer',
+--          ['ia'] = '@parameter.inner',
+--          ['af'] = '@function.outer',
+--          ['if'] = '@function.inner',
+--          ['ac'] = '@class.outer',
+--          ['ic'] = '@class.inner',
+--        },
+--      },
+--      move = {
+--        enable = true,
+--        set_jumps = true, -- whether to set jumps in the jumplist
+--        goto_next_start = {
+--          [']m'] = '@function.outer',
+--          [']]'] = '@class.outer',
+--        },
+--        goto_next_end = {
+--          [']M'] = '@function.outer',
+--          [']['] = '@class.outer',
+--        },
+--        goto_previous_start = {
+--          ['[m'] = '@function.outer',
+--          ['[['] = '@class.outer',
+--        },
+--        goto_previous_end = {
+--          ['[M'] = '@function.outer',
+--          ['[]'] = '@class.outer',
+--        },
+--      },
+--      swap = {
+--        enable = true,
+--        swap_next = {
+--          ['<leader>a'] = '@parameter.inner',
+--        },
+--        swap_previous = {
+--          ['<leader>A'] = '@parameter.inner',
+--        },
+--      },
+--    },
+--  }
+--end, 0)
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
